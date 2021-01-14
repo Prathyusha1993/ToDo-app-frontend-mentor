83711ffb5a0c6aa4e7f25063dfd614e5b83b89f3
prathyusha1993 <pkr131023@gmail.com>
2021-01-13 16:55:27 -0800

    tododata and images

---
+++ src/components/todoData.jsx
@@ -0,0 +1,34 @@
+const todoData = [
+    {
+        id:1,
+        task: 'Complete online React course from scrimba',
+        completed: true
+    },
+    {
+        id:2,
+        task: 'Jog around the park 3x',
+        completed: false
+    },
+    {
+        id:3,
+        task: '10 minutes meditation and 30minutes of yoga',
+        completed: true
+    },
+    {
+        id:4,
+        task: 'Read for 1 hour',
+        completed: false
+    },
+    {
+        id:5,
+        task: 'Pick up groceries',
+        completed: false
+    },
+    {
+        id:6,
+        task: 'Complete Todo app on frontend Mentor',
+        completed: true
+    }
+]
+
+export default todoData;
